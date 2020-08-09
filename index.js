3a781c805e20ada53345a102ff5221a790273b1a
Courtnee McCollum <courtneemeaghan@gmail.com>
2020-08-09 12:43:27 -0500

    Make return value from balance function more descriptive

--- index.js
+++ index.js
@@ -6,27 +6,28 @@ const finances = () => {
   
   const income = (description, amount) => {
     incomeTracker.push({description, amount});
-    return totalIncome += amount;
+    totalIncome += amount;
   }
 
   const deleteIncome = (index) => {
     totalIncome -= incomeTracker[index].amount;
     incomeTracker.splice(index, 1);
-    return totalIncome;
+    totalIncome;
   }
 
   const expenses = (description, amount) => {
     expenseTracker.push({description, amount});
-    return totalExpenses += amount;
+    totalExpenses += amount;
   }
 
   const deleteExpense = (index) => {
     totalExpenses -= expenseTracker[index].amount;
     expenseTracker.splice(index, 1);
-    return totalExpenses;
   }
 
-  const balance = () => totalIncome - totalExpenses;
+  const balance = () => {
+    return `Income: $${totalIncome} | Expenses: $${totalExpenses} | Balance: $${totalIncome - totalExpenses}`;
+  }
 
   return {
     income,
