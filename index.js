4ceeed69b70be145e23bd8ace5dae71121c9581e
Courtnee McCollum <courtneemeaghan@gmail.com>
2020-08-09 12:32:32 -0500

    Add descriptions to income and expenses

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
 const finances = () => {
+  let incomeTracker = [];
+  let expenseTracker = [];
   let totalIncome = 0;
   let totalExpenses = 0;
   
-  const income = (amount) => totalIncome += amount;
+  const income = (description, amount) => {
+    incomeTracker.push({description, amount});
+    return totalIncome += amount;
+  }
 
-  const deleteIncome = (amount) => totalIncome -= amount;
+  const deleteIncome = (index) => {
+    totalIncome -= incomeTracker[index].amount;
+    incomeTracker.splice(index, 1);
+    return totalIncome;
+  }
 
-  const expenses = (amount) => totalExpenses += amount;
+  const expenses = (description, amount) => {
+    expenseTracker.push({description, amount});
+    return totalExpenses += amount;
+  }
 
-  const deleteExpense = (amount) => totalExpenses -= amount;
+  const deleteExpense = (index) => {
+    totalExpenses -= expenseTracker[index].amount;
+    expenseTracker.splice(index, 1);
+    return totalExpenses;
+  }
 
   const balance = () => totalIncome - totalExpenses;
 
